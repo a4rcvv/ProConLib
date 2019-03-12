c1974c7dce117821262661332bd9398a14c98c65
Arc <arcslabmail@gmail.com>
2019-03-12 23:18:00 +0900

    make class more faster. T hoge-> const T &hoge

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -24,64 +24,65 @@ class SegmentTree {
   // the data of SegTree. implemented as array.
   std::vector<T> tree_;
   // call Compare(int,int,int) recursively.
-  void Merge(int parent);
+  void Merge(const int &parent);
   // return LeftChild's position from parent's position.
-  int LeftChild(int parent);
+  int LeftChild(const int &parent);
   // RightChild version
-  int RightChild(int parent);
+
+  int RightChild(const int &parent);
   // return parent's position from left/right child's position.
-  int Parent(int child);
+  int Parent(const int &child);
 
   // The function which is used to answer a query internally.
-  T PrivateQuery(int query_left, int query_right, int node, int node_left,
-                 int node_right);
+  T PrivateQuery(const int &query_left, const int &query_right, const int &node,
+                 const int &node_left, const int &node_right);
 
   // Compare two query and return the result.
   // This function is used to answer a query.
   // You must override this function.
-  virtual T CompareQuery(T left, T right) = 0;
+  virtual T CompareQuery(const T &left, const T &right) = 0;
   // Compare LeftChild and RightChild,and update parent.
   // You must override this function.
-  virtual T Compare(T left, T right) = 0;
+  virtual T Compare(const T &left, const T &right) = 0;
 
  public:
   // call Initialize().
-  SegmentTree(int n, T initializer);
+  SegmentTree(const int &n, const T &initializer);
   // resize tree_ and initialize tree_ as initializer.
-  void Initialize(int n, T initializer);
+  void Initialize(const int &n, const T &initializer);
   // update pos and its parent, grandparent,
   // great grangparent, great great grandparent, and more...
-  void Update(int pos, T data);
+  void Update(int pos, const T &data);
   // Throw query and get answer.
   // Range is [left,right).
-  T Query(int left, int right);
+  T Query(const int &left, const int &right);
   // output tree(for debugging).
   void OutputTree(void);
 };
 
 template <typename T>
-int SegmentTree<T>::LeftChild(int parent) {
+int SegmentTree<T>::LeftChild(const int &parent) {
   if (range_ == 1) return 0;
   return parent * 2 + 1;
 }
 template <typename T>
-int SegmentTree<T>::RightChild(int parent) {
+int SegmentTree<T>::RightChild(const int &parent) {
   if (range_ == 1) return 0;
   return parent * 2 + 2;
 }
 template <typename T>
-int SegmentTree<T>::Parent(int child) {
+int SegmentTree<T>::Parent(const int &child) {
   if (range_ == 1) return 0;
   return (child - 1) / 2;
 }
 
 template <typename T>
-SegmentTree<T>::SegmentTree(int n, T initializer) {
+SegmentTree<T>::SegmentTree(const int &n, const T &initializer) {
   Initialize(n, initializer);
 }
 
 template <typename T>
-void SegmentTree<T>::Initialize(int n, T initializer) {
+void SegmentTree<T>::Initialize(const int &n, const T &initializer) {
   int64_t temp = 1;
   while (temp < n) temp *= 2;  //要素数を2のべき乗にするといいらしい
   range_       = temp;
@@ -94,25 +95,26 @@ void SegmentTree<T>::Initialize(int n, T initializer) {
 }
 
 template <typename T>
-void SegmentTree<T>::Update(int pos, T data) {
+void SegmentTree<T>::Update(int pos, const T &data) {
   pos += range_ - 1;
   tree_[pos] = data;
   Merge(Parent(pos));
 }
 
 template <typename T>
-void SegmentTree<T>::Merge(int parent) {
+void SegmentTree<T>::Merge(const int &parent) {
   tree_[parent] = Compare(tree_[LeftChild(parent)], tree_[RightChild(parent)]);
   if (parent > 0) Merge(Parent(parent));
 }
 
 template <typename T>
-T SegmentTree<T>::Query(int left, int right) {
+T SegmentTree<T>::Query(const int &left, const int &right) {
   return PrivateQuery(left, right, 0, 0, range_);
 }
 template <typename T>
-T SegmentTree<T>::PrivateQuery(int query_left, int query_right, int node,
-                               int node_left, int node_right) {
+T SegmentTree<T>::PrivateQuery(const int &query_left, const int &query_right,
+                               const int &node, const int &node_left,
+                               const int &node_right) {
   // cerr << query_left << "\t" << query_right << "\t" << node << "\t" <<
   // node_left
   //  << "\t" << node_right << endl;
@@ -141,52 +143,53 @@ void SegmentTree<T>::OutputTree(void) {
   }
 }
 
-// for verifying
-// This code solves AOJ Range Query - Range Minimum Query (RMQ).
-class RMQTree : public SegmentTree<int> {
- private:
-  int CompareQuery(int left, int right);
-  int Compare(int left, int right);
+// // for verifying
+// // This code solves AOJ Range Query - Range Minimum Query (RMQ).
+// class RMQTree : public SegmentTree<int> {
+//  private:
+//   int CompareQuery(const int &left, const int &right);
+//   int Compare(const int &left, const int &right);
 
- public:
-  RMQTree(int n, int initializer) : SegmentTree(n, initializer){};
-};
-int RMQTree::CompareQuery(int left, int right) {
-  if (left < right)
-    return left;
-  else
-    return right;
-}
-int RMQTree::Compare(int left, int right) {
-  if (left < right)
-    return left;
-  else
-    return right;
-}
+//  public:
+//   RMQTree(const int &n, const int &initializer) : SegmentTree(n,
+//   initializer){};
+// };
+// int RMQTree::CompareQuery(const int &left, const int &right) {
+//   if (left < right)
+//     return left;
+//   else
+//     return right;
+// }
+// int RMQTree::Compare(const int &left, const int &right) {
+//   if (left < right)
+//     return left;
+//   else
+//     return right;
+// }
 
-int main(void) {
-  cout << std::fixed << std::setprecision(10);
-  cin.tie(0);
-  std::ios::sync_with_stdio(false);
+// int main(void) {
+//   cout << std::fixed << std::setprecision(10);
+//   cin.tie(0);
+//   std::ios::sync_with_stdio(false);
 
-  int n, q;
-  cin >> n >> q;
-  RMQTree tree(n, INT32_MAX);
-  // tree.OutputTree();
-  for (int i = 0; i < q; i++) {
-    int command, x, y;
-    cin >> command >> x >> y;
+//   int n, q;
+//   cin >> n >> q;
+//   RMQTree tree(n, INT32_MAX);
+//   // tree.OutputTree();
+//   for (int i = 0; i < q; i++) {
+//     int command, x, y;
+//     cin >> command >> x >> y;
 
-    switch (command) {
-      case 0:
-        tree.Update(x, y);
-        // tree.OutputTree();
-        break;
-      case 1:
-        cout << tree.Query(x, y + 1) << endl;
-        break;
-    }
-  }
+//     switch (command) {
+//       case 0:
+//         tree.Update(x, y);
+//         // tree.OutputTree();
+//         break;
+//       case 1:
+//         cout << tree.Query(x, y + 1) << endl;
+//         break;
+//     }
+//   }
 
-  return 0;
-}
+//   return 0;
+// }
