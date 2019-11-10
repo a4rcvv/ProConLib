e56f6712d4f8f65fb47b5311101a24eabbad7fad
Arc <35623957+a4rcvv@users.noreply.github.com>
2019-11-10 19:19:37 +0900

    Create template.cpp
    For ICPC Regional

---
+++ icpc/template.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <cfloat>
+
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define all(x) (x).begin(),(x).end()
+
+int main(){
+    cout<<fixed<<setprecision(16);
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+
+}
